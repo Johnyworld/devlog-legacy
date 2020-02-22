89a16bcadd4304c5903bf8d5ae666414718273bd
Johny Kim <johnyworld@naver.com>
2020-02-22 21:35:03 +0900

    Update Theme toggler rem to em

--- src/Components/Atoms/Buttons/ThemeToggler.tsx
+++ src/Components/Atoms/Buttons/ThemeToggler.tsx
@@ -3,68 +3,69 @@ import styled, { DefaultTheme, StyledComponent } from 'styled-components';
 
 interface ContainerProps {
     isBrightMode: boolean;
-    onClick?: (event:React.MouseEvent<HTMLElement, MouseEvent>) => void;
+    onClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
 }
 
-const Container:StyledComponent<'div', DefaultTheme, ContainerProps, never> = styled.div`
+const Container: StyledComponent<'div', DefaultTheme, ContainerProps, never> = styled.div`
     display: block;
-    width: 4.4rem;
-    height: 2.4rem;
+    font-size: 1rem;
+    width: 4.4em;
+    height: 2.4em;
     border: none;
-    padding: .2rem;
-    border-radius: 2rem;
+    padding: 0.2em;
+    border-radius: 2em;
     outline: none;
-    transition: .3s;
+    transition: 0.3s;
     cursor: pointer;
-    ${(props:ContainerProps)=> props.isBrightMode 
-        ? `
+    ${(props: ContainerProps) =>
+        props.isBrightMode
+            ? `
         background-color: #83aad2;
         `
-        : `
+            : `
         background-color: #000;
-        `
-    };
+        `};
 `;
 
-const Selector:StyledComponent<'div', DefaultTheme, ContainerProps, never> = styled.div`
+const Selector: StyledComponent<'div', DefaultTheme, ContainerProps, never> = styled.div`
     margin: 0 auto;
-    width: 2rem;
-    height: 2rem;
-    border-radius: 1rem;
-    background-color: white;
-    transition: .3s;
-    ${(props:ContainerProps)=> props.isBrightMode 
-        ? `
-        transform: translateX(-.65em);
+    width: 2em;
+    height: 2em;
+    border-radius: 1em;
+    background-color: ${props => props.theme.color.lightest};
+    transition: 0.3s;
+    ${(props: ContainerProps) =>
+        props.isBrightMode
+            ? `
+        transform: translateX(-1em);
         `
-        : `
-        transform: translateX(.65em);
-        `
-    };
+            : `
+        transform: translateX(1em);
+        `};
 `;
 
 interface Props {
     className?: string;
     isBrightMode: boolean;
-    onClick: (event:React.MouseEvent<HTMLElement, MouseEvent>) => void
+    onClick: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
 }
 
 /**
  * ### 테마를 변경하는 버튼입니다.
- * 
+ *
  * - `Dark`모드와 `Bright` 모드가 토글됩니다.
  */
-const ThemeToggler:React.FC<Props> = ({ className, isBrightMode, onClick }) => {
+const ThemeToggler: React.FC<Props> = ({ className, isBrightMode, onClick }) => {
     return (
-        <Container className={className} onClick={onClick} isBrightMode={isBrightMode} >
+        <Container className={className} onClick={onClick} isBrightMode={isBrightMode}>
             <Selector isBrightMode={isBrightMode} />
             {/* <button onClick={onClick}>{ checked ? 'Checked' : 'Unchecked' }</button> */}
         </Container>
     );
-}
+};
 
 ThemeToggler.defaultProps = {
     isBrightMode: true
-}
+};
 
 export default ThemeToggler;
