45b9de849c278427286a4398713da75c9a7c909c
Johny Kim <johnyworld@naver.com>
2020-02-26 20:21:48 +0900

    Change padding in Header

--- src/Components/Organisms/Header/HeaderContainer.tsx
+++ src/Components/Organisms/Header/HeaderContainer.tsx
@@ -10,7 +10,7 @@ const Container = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 2rem;
+    padding: 0 2rem;
 `;
 
 const Column = styled.div`
