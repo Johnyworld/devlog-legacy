4a750353ff2ebdf7ab7432c4cc701c17f45fa011
johnyworld <johnyworld@naver.com>
2020-03-16 01:12:00 +0900

    Remove styled-reset

--- src/Theme/GlobalStyles.ts
+++ src/Theme/GlobalStyles.ts
@@ -1,20 +1,20 @@
-import reset from 'styled-reset';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-    ${reset}
+    html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, main, menu, nav, output, ruby, section, summary, time, mark, audio, video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        box-sizing: border-box;
+        font-family: 'Inter', sans-serif;
+    }
 
     html {
         font-size: 10px;
-        font-family: sans-serif;
     }
 
     body {
         font-size: 1.6rem;
-    }
-
-    * {
-        box-sizing: border-box;
     }
 
     li {
