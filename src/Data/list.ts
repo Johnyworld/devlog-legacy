0c5a637073b99e6bd2ec0f801290f7a61e6cc580
johnyworld <johnyworld@naver.com>
2020-03-27 11:38:19 +0900

    Add storybook link to External link list menu

--- src/Data/list.ts
+++ src/Data/list.ts
@@ -1,19 +1,24 @@
 import { TFunction } from 'i18next';
 
 export const getExternalLinkList = (t: TFunction) => [
-        {
-            id: 'johnyworld',
-            content: t('external_johnyworld'),
-            uri: 'http://johnyworld.com'
-        },
-        {
-            id: '2',
-            content: t('external_github'),
-            uri: 'http://github.com/johnyworld'
-        },
-        {
-            id: '3',
-            content: t('external_codepen'),
-            uri: 'https://codepen.io/johnyworld'
-        }
-    ]
+    {
+        id: 'storybook',
+        content: t('external_storybook'),
+        uri: 'http://johnyworld.com'
+    },
+    {
+        id: 'johnyworld',
+        content: t('external_johnyworld'),
+        uri: 'http://johnyworld.com'
+    },
+    {
+        id: '2',
+        content: t('external_github'),
+        uri: 'http://github.com/johnyworld'
+    },
+    {
+        id: '3',
+        content: t('external_codepen'),
+        uri: 'https://codepen.io/johnyworld'
+    }
+]
--- src/Locale/ko.ts
+++ src/Locale/ko.ts
@@ -7,6 +7,7 @@ export default {
     gnb_work: '포트폴리오',
     gnb_book: '서평',
     external: '외부 링크',
+    external_storybook: '스토리북',
     external_johnyworld: '포트폴리오 사이트',
     external_github: '깃헙',
     external_codepen: '코드펜',
